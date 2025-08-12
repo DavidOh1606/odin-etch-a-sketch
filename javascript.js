c587ec6a973f3f0cb1042ca9d13c36ff0b903dd8
DavidOh1606 <david.oh1606@gmail.com>
2025-08-12 12:51:32 -0700

    Add hover effect

--- javascript.js
+++ javascript.js
@@ -1,17 +1,29 @@
-const SIZE = 16;
-
+const DEFAULT_SIZE = 16;
+const SIZE_LIMIT = 100;
 
 const container = document.querySelector("#container");
 
+createGrid(DEFAULT_SIZE);
 
-for (let row = 0; row < SIZE; row++) {
-    const rowContainer = document.createElement("div");
-    rowContainer.classList.add("row");
 
-    for (let col = 0; col < SIZE; col++) {
-        const grid = document.createElement("div");
-        grid.classList.add("grid");
-        rowContainer.appendChild(grid);
+
+function createGrid(size) {
+
+    for (let row = 0; row < size; row++) {
+        const rowContainer = document.createElement("div");
+        rowContainer.classList.add("row");
+
+        for (let col = 0; col < size; col++) {
+            const grid = document.createElement("div");
+            grid.classList.add("grid");
+            grid.addEventListener("mouseover", setHovered);
+            rowContainer.appendChild(grid);
+        }
+        container.appendChild(rowContainer);
     }
-    container.appendChild(rowContainer);
+}
+
+function setHovered(e) {
+    e.target.classList.add("hovered");
+    e.target.removeEventListener("mouseover", setHovered);
 }
